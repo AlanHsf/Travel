ee797dac72135bdeae823c8c269bbb28f8edd88d
AlanH <1076739300@qq.com>
2018-12-19 22:41:34 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" >
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img"  :src="item.imgUrl"/>
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/9e/8bca9a1d513cd802.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/9e/8bca9a1d513cd802.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/9e/8bca9a1d513cd802.jpg'
+      }, {
+        id: '0004',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/9e/8bca9a1d513cd802.jpg'
+      }, {
+        id: '0005',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1809/9e/8bca9a1d513cd802.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background : #fff
+  .wrapper
+    overflow : hidden
+    width : 100%
+    hight : 0
+    padding-bottom : 31.25%
+    background : #eee
+    .swiper-img
+      width : 100%
+</style>
