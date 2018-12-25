8353b47f6ed173e64dab491b3068401912ead602
AlanH <1076739300@qq.com>
2018-12-25 16:40:54 +0800

    add city-keepalive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <!-- router-view是显示的当前路由地址所对应的内容 -->
-    <router-view/>
+    <keep-alive>
+      <!-- router-view是显示的当前路由地址所对应的内容 -->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
