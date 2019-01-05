586bba0a838174865549ec35acb3db3c546f9af0
AlanH <1076739300@qq.com>
2019-01-05 15:27:08 +0800

    en

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
 import Detail from '@/pages/detail/Detail'
+import Router from 'vue-router'
 
 Vue.use(Router)
 
